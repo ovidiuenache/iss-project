939003bf19f31a4844a7e59d05943ce28c37be79
Vlad <Vlad>
2017-05-08 05:11:17 +0300

    backed login till repository

---
+++ App/App/Controller/ControllerServiceLogin.cs
@@ -0,0 +1,45 @@
+﻿using App.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Controller
+{
+    /// <summary>
+    /// 
+    /// The class ControllerServiceLogin for LoginForm
+    /// 
+    /// Author: Vancea Vlad
+    /// </summary>
+    /// 
+    public class ControllerServiceLogin
+    {
+        public ControllerServiceLogin() { }
+
+        public bool areCredentialsValid(string username, string password)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+        public bool isConferenceActive()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<string> getRoleOfUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User findByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- App/App/UI/Login.cs
+++ App/App/UI/Login.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using App.Controller;
+using App.Entity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,12 @@ namespace App
 {
     public partial class Login : Form
     {
-        public Login()
+        ControllerServiceLogin controllerLogin;
+        public Login(ControllerServiceLogin controllerLogin)
         {
             InitializeComponent();
+            this.controllerLogin = controllerLogin;
+       
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -29,7 +34,38 @@ namespace App
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            string userName = this.UserNameTextBox.Text;
+            string password = this.PasswordTextBox.Text;
 
+            User user = controllerLogin.findByUsername(userName);
+            Role chairRole = new Role("Chair", "chair");
+
+            if (!userName.Equals("") && !password.Equals(""))
+            {
+                if (controllerLogin.areCredentialsValid(userName,password) == true)
+                {
+                    if(controllerLogin.getRoleOfUser(user).Contains(chairRole.Title))
+                    {
+                        if (controllerLogin.isConferenceActive()==true)
+                        {
+                            //redirect user to phase1 page;
+                            this.Close();
+                        }
+                        else
+                        {
+                            //redirect chairs to prelimianry page
+                            this.Close();
+                        }
+                    }
+                    else                    
+                        if (controllerLogin.isConferenceActive() == false)                        
+                            MessageBox.Show("Restrict acces", "This conference is not active");                       
+                }
+                else
+                    MessageBox.Show("The data are not valid");                
+            }
+            else
+                MessageBox.Show("The textboxes can not be empty");
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
