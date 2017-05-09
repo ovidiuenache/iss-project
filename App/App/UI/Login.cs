dc63ce99e0b4b4dbf4a14436c36a6676dd0dada6
Ovidiu Enache <i.ovidiuenache@yahoo.com>
2017-05-09 21:18:52 +0300

    Added backend for login button

--- App/App/UI/Login.cs
+++ App/App/UI/Login.cs
@@ -1,5 +1,7 @@
-﻿using App.Entity;
+﻿using App.Controller;
+using App.Entity;
 using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace App
@@ -8,13 +10,15 @@ namespace App
     {
         Form parentForm;
         Conference activeConference;
+        LoginController loginController;
 
-        public Login(Conference activeConference, Form parentForm)
+        public Login(Conference activeConference, Form parentForm, LoginController loginController)
         {
             InitializeComponent();
 
             this.activeConference = activeConference;
             this.parentForm = parentForm;
+            this.loginController = loginController;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -29,7 +33,38 @@ namespace App
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            string username = this.UserNameTextBox.Text;
+            string password = this.PasswordTextBox.Text;
 
+            User user = loginController.findByUsername(username);
+            Role chairRole = new Role("Chair", "chair");
+
+            try
+            {
+                loginController.areCredentialsValid(username, password);
+
+                if (loginController.isConferenceActive() == true)
+                {
+                    //redirect user to phase active page;
+                    this.Close();
+                }
+                else
+                {
+                    if (loginController.getUserRole(user).Contains(chairRole.Title))
+                    {
+                        //redirect chairs to preliminary page
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Restrict aces for non chairs");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("The credentials are not valid");
+            }
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
