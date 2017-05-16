0b225cb9e10ce28c6d5c009164e7caccb24198a7
RamonaVint <vint.ramona@yahoo.com>
2017-05-16 10:12:26 +0300

    Dropdown list to Create and Update Conference

--- App/App/UI/UpdateConference.cs
+++ App/App/UI/UpdateConference.cs
@@ -1,5 +1,6 @@
 ﻿using App.Entity;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -22,9 +23,24 @@ namespace App.UI
             this.parent = parent;
         }
 
+        public UpdateConference()
+        {
+            InitializeComponent();
+        }
+
         private void UpdateConferenceUI_Load(object sender, EventArgs e)
         {
+            var listTopic = populateList();
 
+            foreach (var topic in listTopic)
+            {
+                comboBoxCheckedListUpdate.Items.Add(topic);
+            }
+            // If more then 5 items, add a scroll bar to the dropdown.
+            comboBoxCheckedListUpdate.MaxDropDownItems = 5;
+            // Make the "Name" property the one to display
+            comboBoxCheckedListUpdate.DisplayMember = "Name";
+            comboBoxCheckedListUpdate.ValueSeparator = ", ";
         }
 
         private void buttonBack_Click(object sender, EventArgs e)
@@ -54,6 +70,20 @@ namespace App.UI
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private List<Topic> populateList()
+        {
+            var listTest = new List<Topic>();
+            listTest.Add(new Topic("name1", "desc1"));
+            listTest.Add(new Topic("name2", "desc2"));
+            listTest.Add(new Topic("name3", "desc3"));
+            listTest.Add(new Topic("name4", "desc4"));
+            listTest.Add(new Topic("name5", "desc5"));
+            listTest.Add(new Topic("name6", "desc6"));
+            listTest.Add(new Topic("name7", "desc7"));
+
+            return listTest;
         }
     }
 }
