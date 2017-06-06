b72c62a7d251472e853c1ae576cea87cc00dc339
Claudiu G. Nicola <claudiunicola96@gmail.com>
2017-06-06 03:02:10 +0300

    back instead of logout

--- App/App/UI/PhaseThree/ChooseRoom.Designer.cs
+++ App/App/UI/PhaseThree/ChooseRoom.Designer.cs
@@ -81,7 +81,7 @@
             this.buttonBack.Name = "buttonBack";
             this.buttonBack.Size = new System.Drawing.Size(137, 48);
             this.buttonBack.TabIndex = 6;
-            this.buttonBack.Text = "Logout";
+            this.buttonBack.Text = "Back";
             this.buttonBack.UseVisualStyleBackColor = true;
             this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
             // 
